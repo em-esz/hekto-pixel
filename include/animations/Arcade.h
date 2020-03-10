526b9a47159ba426d4a35fbad47d4c76b74f9fc3
adam.czyszczon <adam.czyszczon@gmail.com>
2020-03-10 16:35:23 +0100

    Fixed Mario performance problems.

--- include/animations/Arcade.h
+++ include/animations/Arcade.h
@@ -4,13 +4,13 @@
 class Arcade: public Animation {
     private:
         int currentFrame = 0;
-        CRGB backgroundColor PROGMEM = CRGB(0, 77, 0);
+        CRGB backgroundColor;
         Mario mario;
 
     public:
         Arcade() : Animation() {
             name = "arcade";
-            interval = 150;
+            interval = 120;
         }
 
         boolean configure(JsonDocument &json) {
@@ -26,16 +26,14 @@ class Arcade: public Animation {
         }
         
         boolean renderFrame(Canvas &canvas) {
-            currentFrame++;
             for (int16_t x = 0; x < canvas.width(); x++) {
                 for (int16_t y = 0; y < canvas.height(); y++) {
-                    uint32_t color = pgm_read_dword(&mario.getFrames()[currentFrame-1][y][x]);
+                    uint32_t color = mario.getPixelColor(currentFrame, y, x);
                     drawPixel(x, y, canvas, color);
                 }
             }
-            if (currentFrame > 3) {
-                currentFrame = 0;
-            } 
+            currentFrame++;
+            restartAnimation();
 
             return true;
         }
@@ -45,6 +43,12 @@ class Arcade: public Animation {
                 canvas.drawPixel(x, y, canvas.Color24to16(pixelColor));
             } else {
                 canvas.drawPixel(x, y, canvas.CRGBtoint32(pgm_read_dword(&this->backgroundColor)));
+            }
+        }
+
+        void restartAnimation() {
+            if (currentFrame > mario.getTotalFramesNumber() - 1) {
+                currentFrame = 0;
             }
         }
 };
--- include/animations/arcade/ArcadeModel.h
+++ include/animations/arcade/ArcadeModel.h
@@ -1,9 +1,12 @@
 #include "HektoPixel.h"
 
-typedef std::vector<std::vector<std::vector<uint32_t>>> Frames;
+using namespace std;
+
+typedef vector<vector<vector<uint32_t>>> ArcadeFrames;
 
 class ArcadeModel {
     public:
         virtual uint32_t getBackgroundColor() = 0;
-        virtual Frames getFrames() = 0;
+        virtual uint32_t getPixelColor(int16_t frame, int16_t column, int16_t row) = 0;
+        virtual int getTotalFramesNumber() = 0;
 };
